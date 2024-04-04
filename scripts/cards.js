855aaa545ac0981301187ee4d235d81667e58d20
Nikita Maltsev <djenot330@mail.ru>
2024-04-04 12:42:21 +0300

    Создал карточки

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,85 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const initialCards1 = [
+  {
+    name: "Porshe",
+    link: "images/card.JPG",
+  },
+  {
+    name: "Porshe",
+    link: "images/card2.JPG",
+  },
+  {
+    name: "Porshe",
+    link: "images/card3.JPG",
+  },
+  {
+    name: "Porshe",
+    link: "images/card4.JPG",
+  },
+  {
+    name: "Porshe",
+    link: "images/card5.JPG",
+  },
+  {
+    name: "Porshe",
+    link: "images/card6.JPG",
+  },
+  {
+    name: "Porshe",
+    link: "images/card7.JPG",
+  },
+  {
+    name: "Porshe",
+    link: "images/card8.JPG",
+  },
+  {
+    name: "Porshe",
+    link: "images/card9.JPG",
+  },
+  {
+    name: "Ram",
+    link: "images/card10.JPG",
+  },
+  {
+    name: "Ram",
+    link: "images/card11.JPG",
+  },
+  {
+    name: "BMW",
+    link: "images/card12.JPG",
+  },
+  {
+    name: "BMW",
+    link: "images/card13.JPG",
+  },
+  {
+    name: "BMW",
+    link: "images/card14.JPG",
+  },
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,54 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content
 // @todo: DOM узлы
 
+
+
 // @todo: Функция создания карточки
+
+// function createCard(cardData) {
+//     const templateContent = cardTemplate.content.cloneNode(true);
+//     const cardList = document.querySelector('.places__item');
+//     const image = cardList.querySelector('.card__image');
+//     const title = cardList.querySelector('.card__title');
+    
+// }
+
+// const cardContainer = document.querySelector('.card')
+
+// initialCards1.forEach(function())
+
+function createCard(cardData) {
+    // Получаем доступ к шаблону
+    const template = document.querySelector('#card-template');
+  
+    // Клонируем содержимое шаблона
+    const templateContent = template.content.cloneNode(true);
+  
+    // Заполняем клонированный шаблон данными из объекта cardData
+    const cardImage = templateContent.querySelector('.card__image');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+  
+    const cardTitle = templateContent.querySelector('.card__title');
+    cardTitle.textContent = cardData.name;
+  
+    // Возвращаем заполненный шаблон
+    return templateContent;
+  }
+  
+  // Пример использования функции для создания карточек из массива initialCards1
+  const placesList = document.querySelector('.places__list');
+  initialCards1.forEach(function(cardData) {
+    const cardElement = createCard(cardData);
+    placesList.appendChild(cardElement);
+  });
+  
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+
